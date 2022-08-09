c8f5de8bc3c215a7c7777cc40c3dd1ee016b9153
leesan195159 <sniping1997@naver.com>
2022-08-09 13:18:44 +0900

    first setting

---
+++ my_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'practice',
+        'USER': 'root',
+        'PASSWORD': '0000',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+SECRET_KEY = 'django-insecure-1ud*8ma5j8y=g_5jab4)_#)@#-s$t590h7giokw8u$@zhvvub3'
